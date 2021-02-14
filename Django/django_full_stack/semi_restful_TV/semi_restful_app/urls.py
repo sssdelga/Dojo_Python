d3cb33a73a510018e44f25c614bf73e1b88b1143
Samantha Delgado <sss.delgad@gmail.com>
2021-02-14 13:30:57 -0500

    semi-restful TV shows

---
+++ Django/django_full_stack/semi_restful_TV/semi_restful_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('new', views.new_show),
+    path('<int:show_id>', views.view_show),
+    path('<int:show_id>/edit', views.edit_show),
+    path('<int:show_id>/destroy', views.delete_show)
+]
+
---
+++ Django/django_full_stack/semi_restful_TV/semi_restful_app/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from .models import Show
+
+def index(request):
+    context = {
+        'shows': Show.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def new_show(request):
+    if request.method=='GET':
+        return render(request, 'new_show.html')
+    else:
+        Show.objects.create(
+            title = request.POST['title'],
+            network = request.POST['network'],
+            release_date = request.POST['release_date'],
+            description = request.POST['description']
+        )
+        return redirect('/shows')
+
+def view_show(request, show_id):
+    context ={
+        'show': Show.objects.get(id=show_id)
+    }
+    
+    return render(request, 'show_id.html', context)
+
+def edit_show(request, show_id):
+    if request.method=='GET':
+        context = {
+            'show': Show.objects.get(id=show_id)
+        }
+        return render(request, 'edit_show.html', context)
+    else:
+        edit = Show.objects.get(id=show_id)
+        print(edit)
+        if edit.title == True:
+            edit.title = request.POST['title']
+
+        if edit.network == True:
+            edit.network = request.POST['network']
+
+        if edit.release_date == True:
+            edit.release_date = request.POST['release_date']
+
+        if edit.description == True:
+            edit.description = request.POST['description']
+        
+        return redirect('/shows/<int:show_id>')
+
+def delete_show(request, show_id):
+    show_to_delete = Show.objects.get(id=show_id)
+    show_to_delete.delete()
+    return redirect('/shows')
